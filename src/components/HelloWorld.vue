c3a06ba713145a6e9036da186e754e9ad3d1191c
Poamrong Rith <poo@Poamrongs-MBP.attlocal.net>
2018-12-17 14:09:50 -0800

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'This application is run on Digital Ocean, and Node.js.'
     }
   }
 }
